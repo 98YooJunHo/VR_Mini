092d566670a22a99c6f6918a3024efeead08f028
98YooJunHo <134994603+98YooJunHo@users.noreply.github.com>
2023-10-19 11:14:30 +0900

    fix: HUD가 카메라의 로테이션을 따라서 이동하도록 수정

---
+++ VR_Mini/Assets/Yoo/Scripts/HUD_FollowCam_Yoo.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUD_FollowCam_Yoo : MonoBehaviour
+{
+    private GameObject centerEyeObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        centerEyeObj = GameObject.Find("CenterEyeAnchor");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!GameManager.Instance.gameOver)
+        {
+            transform.rotation = centerEyeObj.transform.rotation;
+        }
+    }
+}
